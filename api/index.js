3184b44773d4feb0e8ca26663b7901f08a35df45
vineeth kumar <vineethkumargaddam143@gmail.com>
2023-12-29 16:44:40 +0530

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,19 @@
-import express from 'express'
-const app=express();
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("mongodb is connected!!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+const app = express();
 const port = process.env.port || 5000;
-app.listen(port,()=>{
-    console.log(`node server is stared at port: ${port}`);
-    
-})
+app.listen(port, () => {
+  console.log(`node server is stared at port: ${port}`);
+});
