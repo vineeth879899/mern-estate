91c22845ce25e0147e2d8a6f626bd5428dbdc6e4
vineeth kumar <vineethkumargaddam143@gmail.com>
2023-12-30 10:42:57 +0530

    complete sign up page functionalty

--- client/src/pages/SinOut.jsx
+++ client/src/pages/SinOut.jsx
@@ -1,15 +1,57 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 
 export default function SinOut() {
+  const [formData, setformData]=useState({});
+  const [error, setError]=useState(null);
+  const [loading ,setLoading]=useState(false);
+  const navigation=useNavigate();
+  const handleChange=(e)=>{
+    setformData(
+      {
+        ...formData,
+        [e.target.id]:e.target.value,
+      });
+  }
+  
+  const handlerSubmit =async (e) => {
+    e.preventDefault();
+    try{
+    setLoading(true);
+    const res=await fetch('/api/auth/signup',{
+      method:'POST',
+      headers:{
+        'Content-Type':'application/json',
+      },
+      body:JSON.stringify(formData), 
+    });
+    const data=await res.json();
+    console.log(data);
+    if(data.success === false){
+      setLoading(false);
+      setError(data.message);
+      return;
+    }
+    setLoading(false);
+    setError(null);
+    navigation('/sinin');
+    
+  }catch(error){
+     setLoading(false);
+     setError(error.message);
+  }
+  };
+
+  //console.log(formData);
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>Sign Up</h1>
-      <form className='flex flex-col gap-4'>
-        <input type='text' placeholder='username' className='border p-3 rounded-lg' id='username'></input>
-        <input type='email' placeholder='email' className='border p-3 rounded-lg' id='email'></input>
-        <input type='password' placeholder='password' className='border p-3 rounded-lg' id='password'></input>
-        <button className='bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-65'>Sign Up</button>
+      
+      <form onSubmit={handlerSubmit} className='flex flex-col gap-4'>
+        <input type='text' placeholder='username' className='border p-3 rounded-lg' id='username' onChange={handleChange}></input>
+        <input type='email' placeholder='email' className='border p-3 rounded-lg' id='email' onChange={handleChange}></input>
+        <input type='password' placeholder='password' className='border p-3 rounded-lg' id='password' onChange={handleChange}></input>
+        <button className='bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-65'>{loading ?'Loading...':'sign up'}</button>
       </form>
       <div className='flex gap-4 mt-3'>
         Have an Account
@@ -17,6 +59,7 @@ export default function SinOut() {
           <span className='text-blue-700'>Sign In</span>
           </Link>
       </div>
+      {error && <p className='text-red-500 mt-5'>{error}</p>}
     </div>
   )
 }
