070d5e05318e1fd3c5ea511aa0b63a7fcb1b26bb
vineeth kumar <vineethkumargaddam143@gmail.com>
2023-12-30 11:56:28 +0530

    create sign in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.models.js";
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from "../utils/error.js";
+import jwt from 'jsonwebtoken';
 
 export const signup=async(req,res,next)=>{
     const {username,email,password}=req.body;
@@ -13,5 +14,26 @@ export const signup=async(req,res,next)=>{
     catch(error){
         //res.status(500).json(error.message);
         next(error);
+    }
+}
+
+export const signin=async(req,res,next)=>{
+    const {email,password}=req.body;
+    try{
+       const vaildUser=await User.findOne({email});
+       if(!vaildUser)
+       return next(errorHandler(404,'user not found!!'));
+    const vaildPasswords= bcryptjs.compareSync(password,vaildUser.password);
+    if(!vaildPasswords)
+    return next(errorHandler(401,'Wrong credetials!!!'));
+    const token = jwt.sign({id:vaildUser._id},process.env.JWT_SECRET);
+    const {password:pass, ...rest}=vaildUser._doc;
+    res
+    .cookie('access_token',token,{httpOnly:true})
+    .status(200)
+    .json(rest);
+
+    }catch(error){
+       next(error);
     }
 }
