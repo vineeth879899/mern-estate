2a06c330d6d2ef5eed8052211ecfecc0c99018ce
vineeth kumar <vineethkumargaddam143@gmail.com>
2024-01-16 22:51:08 +0530

    add delete user functionality

--- api/routers/user.router.js
+++ api/routers/user.router.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { test ,updateUser} from '../controllers/user.controller.js';
+import { deleteUser, test ,updateUser} from '../controllers/user.controller.js';
 import { veriftToken } from '../utils/veriftUser.js';
 
 const router=express.Router();
 
 router.get('/test',test)
 router.post('/update/:id',veriftToken, updateUser)
+router.delete('/delete/:id',veriftToken, deleteUser)
 
 export default router;
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -11,6 +11,9 @@ import {
   updateUserStart,
   updateUserSuccess,
   updateUserFailure,
+  deleteUserFailure,
+  deleteUserStart,
+  deleteUserSuccess,
 } from '../redux/user/UserSlice';
 import { useDispatch } from 'react-redux';
 
@@ -94,6 +97,24 @@ export default function Profile() {
       dispatch(updateUserFailure(error.message));
     }
   };
+  const handleDeleteUser = async () => {
+    try{
+          dispatch(deleteUserStart());
+          const res = await fetch(`api/user/delete/${currentUser._id}`,{
+            method:'DELETE',
+          });
+          const data = await res.json();
+      if (data.success === false) {
+        dispatch(deleteUserFailure(data.message));
+        return;
+      }
+      dispatch(deleteUserSuccess(data));
+
+    }
+    catch(error){
+       dispatch(deleteUserFailure(error.message))
+    }
+  }
 
   return (
     <div className='p-3 max-w-lg mx-auto'>
@@ -151,7 +172,7 @@ export default function Profile() {
         </button>
       </form>
       <div className='flex justify-between mt-5'>
-        <span className='text-red-700'>Delete account</span>
+        <span onClick={handleDeleteUser} className='text-red-700'>Delete account</span>
         <span className='text-red-700'>Sign out</span>
       </div>
       <p className='text-red-700 mt-5'>{error ? error : ''}</p>
